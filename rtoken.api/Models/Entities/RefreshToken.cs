2c3be33925c7895b9dbb5060ccd4b1bc874b01c1
Luis J. Montes <luisjavm3@gmail.com>
2022-06-06 17:12:43 -0500

    Fix testing issue

--- rtoken.api/Models/Entities/RefreshToken.cs
+++ rtoken.api/Models/Entities/RefreshToken.cs
@@ -6,7 +6,7 @@ namespace rtoken.api.Models.Entities
         public string Value { get; set; }
         public DateTime CreatedAt { get; } = DateTime.UtcNow;
         public string CreatedByIp { get; set; }
-        public DateTime ExpiresAt { get; }
+        public DateTime ExpiresAt { get; set; }
         public DateTime? RevokedAt { get; set; }
         public string? RevokedByIp { get; set; }
         public string? ReasonRevoked { get; set; }
