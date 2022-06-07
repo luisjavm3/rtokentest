7e5a46d9b6f31429ddbf3d3a3e1cd818b844f8ff
Luis J. Montes <luisjavm3@gmail.com>
2022-06-07 10:01:37 -0500

    Implement RegisterResponse

--- rtoken.api/DTOs/Auth/RegisterResponse.cs
+++ rtoken.api/DTOs/Auth/RegisterResponse.cs
@@ -1,7 +1,14 @@
+using System.Text.Json.Serialization;
+
 namespace rtoken.api.DTOs.Auth
 {
     public class RegisterResponse
     {
-        
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string token { get; set; }
+        [JsonIgnore]
+        public string RefreshToken { get; set; }
+
     }
 }
