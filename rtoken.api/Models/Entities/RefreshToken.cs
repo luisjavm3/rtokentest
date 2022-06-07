e8d5c13f1f4fb6c6bba72c238067cba36e24694b
Luis J. Montes <luisjavm3@gmail.com>
2022-06-07 15:21:20 -0500

    Modify RefreshToken entity

--- rtoken.api/Models/Entities/RefreshToken.cs
+++ rtoken.api/Models/Entities/RefreshToken.cs
@@ -6,11 +6,12 @@ namespace rtoken.api.Models.Entities
         public string Value { get; set; }
         public DateTime CreatedAt { get; } = DateTime.UtcNow;
         public string CreatedByIp { get; set; }
-        public DateTime ExpiresAt { get; set; }
+        public DateTime ExpiresAt { get; set; } = DateTime.UtcNow.AddDays(1);
         public DateTime? RevokedAt { get; set; }
-        public string RevokedByIp { get; set; }
-        public string ReasonRevoked { get; set; }
+        public string RevokedByIp { get; set; } = string.Empty;
+        public string ReasonRevoked { get; set; } = string.Empty;
         public string TokenSession { get; set; }
+        public User User { get; set; }
         public bool IsRevoked => RevokedAt == null ? false : true;
         public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
     }
