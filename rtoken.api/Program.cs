965923c3a0a7fec702afdc675ca84bec7659512a
Luis J. Montes <luisjavm3@gmail.com>
2022-06-07 10:19:05 -0500

    Add DataContext to application services

--- rtoken.api/Program.cs
+++ rtoken.api/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using rtoken.api.Data;
 using rtoken.api.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
