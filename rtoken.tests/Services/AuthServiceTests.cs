7ec92b6653b0d827bfb9ddb625d2505f94ae865f
Luis J. Montes <luisjavm3@gmail.com>
2022-06-13 09:19:29 -0500

    Comment unimplemented test method

--- rtoken.tests/Services/AuthServiceTests.cs
+++ rtoken.tests/Services/AuthServiceTests.cs
@@ -1,17 +1,22 @@
 using rtoken.api.Data;
 using Xunit;
 using Moq;
+using Microsoft.EntityFrameworkCore;
+using rtoken.api.Models.Entities;
+using System.Linq;
 
 namespace rtoken.tests.Services
 {
     public class AuthServiceTests
     {
-        [Fact]
-        public void Register_WhenUserAlreadyExists_ThrowsAppException()
-        {
-            var dataMock = new Mock<DataContext>();
-
-
-        }
+        // [Fact]
+        // public void No_Name()
+        // {
+        //     var mockDbSet = new Mock<DbSet<RefreshToken>>();
+        //     mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.Provider).Returns(data.Provider);
+        //     mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.Expression).Returns(data.Expression);
+        //     mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.ElementType).Returns(data.ElementType);
+        //     mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+        // }
     }
 }
