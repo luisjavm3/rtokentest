9927249979bd271f59855076303602eda6522239
Luis J. Montes <luisjavm3@gmail.com>
2022-06-13 11:39:02 -0500

    Implement RevokeRToken service and controller

--- rtoken.tests/Services/AuthServiceTests.cs
+++ rtoken.tests/Services/AuthServiceTests.cs
@@ -9,14 +9,14 @@ namespace rtoken.tests.Services
 {
     public class AuthServiceTests
     {
-        // [Fact]
-        // public void No_Name()
-        // {
-        //     var mockDbSet = new Mock<DbSet<RefreshToken>>();
-        //     mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.Provider).Returns(data.Provider);
-        //     mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.Expression).Returns(data.Expression);
-        //     mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.ElementType).Returns(data.ElementType);
-        //     mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-        // }
+        [Fact]
+        public void No_Name()
+        {
+            var mockDbSet = new Mock<DbSet<RefreshToken>>();
+            mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockDbSet.As<IQueryable<RefreshToken>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            // }
+        }
     }
-}
