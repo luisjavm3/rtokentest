d28f74db0c82800ea584d5d3effcbf326cd80864
Luis J. Montes <luisjavm3@gmail.com>
2022-06-14 20:41:09 -0500

    Implement AuthService test

--- rtoken.tests/Fixtures/TestData.cs
+++ rtoken.tests/Fixtures/TestData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using rtoken.api.Models.Entities;
@@ -15,11 +16,32 @@ namespace rtoken.tests.Fixtures
             {
                 return new List<RefreshToken>
                 {
-                    new RefreshToken{Id=1, Value="refresh-token-value-1"},
-                    new RefreshToken{Id=2, Value="refresh-token-value-2"},
-                    new RefreshToken{Id=3, Value="refresh-token-value-3"},
-                    new RefreshToken{Id=4, Value="refresh-token-value-4"},
-                    new RefreshToken{Id=5, Value="refresh-token-value-5"},
+                    // ↓↓↓ Expired but not revoked.
+                    new RefreshToken{
+                        Id=1,
+                        Value="refresh-token-value-1",
+                        ExpiresAt=DateTime.UtcNow.AddMinutes(-1),
+                        RevokedAt = null,
+                    },
+                    // ↓↓↓ Expired AND revoked.
+                    new RefreshToken{
+                        Id=2,
+                        Value="refresh-token-value-2",
+                        ExpiresAt=DateTime.UtcNow.AddMinutes(-1),
+                        RevokedAt=DateTime.UtcNow.AddMinutes(-1)
+                    },
+                    new RefreshToken{
+                        Id=3,
+                        Value="refresh-token-value-3",
+                    },
+                    new RefreshToken{
+                        Id=4,
+                        Value="refresh-token-value-4",
+                    },
+                    new RefreshToken{
+                        Id=5,
+                        Value="refresh-token-value-5",
+                    },
                 }
                 .AsQueryable();
             }
--- rtoken.tests/Services/AuthServiceTests.cs
+++ rtoken.tests/Services/AuthServiceTests.cs
@@ -9,6 +9,9 @@ using rtoken.api.Models.TokensManager;
 using rtoken.api.Models;
 using Microsoft.AspNetCore.Http;
 using FluentAssertions;
+using System.Linq;
+using System;
+using System.Collections;
 
 namespace rtoken.tests.Services
 {
@@ -42,5 +45,87 @@ namespace rtoken.tests.Services
                 .ThrowAsync<AppException>()
                 .WithMessage("Invalid token.");
         }
+
+        [Fact]
+        public async void RefreshToken_ThorwsAppException_WhenFindingExpiredButNotRevokedRefreshToken()
+        {
+            // Arrange
+            var dataContext = new Mock<DataContext>();
+            var aTokenManage = new Mock<IAccessTokenManager>();
+            var rTokenManager = new Mock<IRefreshTokenManager>();
+            var httpContextAccessor = new Mock<IHttpContextAccessor>();
+
+            var rTokenDbSet = TestFunctions.GetDbSet<RefreshToken>(TestData.RefreshTokens).Object;
+
+            dataContext
+                .Setup(x => x.RefreshTokens)
+                .Returns(rTokenDbSet);
+
+            httpContextAccessor
+                .Setup(x => x.HttpContext.Request.Headers.ContainsKey(It.IsAny<string>()))
+                .Returns(true);
+
+            var sut = new AuthService
+                        (
+                            dataContext.Object,
+                            aTokenManage.Object,
+                            rTokenManager.Object,
+                            httpContextAccessor.Object
+                        );
+
+            await sut
+                .Awaiting(x => x.RefreshToken("refresh-token-value-1"))
+                .Should()
+                .ThrowAsync<AppException>()
+                .WithMessage("Token expired.");
+
+            await sut
+                .Awaiting(x => x.RefreshToken("refresh-token-value-2"))
+                .Should()
+                .NotThrowAsync<AppException>();
+
+            await sut
+                .Awaiting(x => x.RefreshToken("refresh-token-value-3"))
+                .Should()
+                .NotThrowAsync<AppException>();
+        }
+
+        // [Fact]
+        // public async void RefreshToken_ThorwsAppException_WhenFindingRevokedRefreshToken()
+        // {
+        //     // Arrange
+        //     var dataContext = new Mock<DataContext>();
+        //     var aTokenManage = new Mock<IAccessTokenManager>();
+        //     var rTokenManager = new Mock<IRefreshTokenManager>();
+        //     var httpContextAccessor = new Mock<IHttpContextAccessor>();
+
+        //     var rTokenDbSet = TestFunctions.GetDbSet<RefreshToken>(TestData.RefreshTokens);
+
+        //     dataContext
+        //         .Setup(x => x.RefreshTokens)
+        //         .Returns(rTokenDbSet.Object);
+        //     // dataContext
+        //     //     .Setup(x => x.RefreshTokens.Where(It.IsAny<Func<object, bool>>()))
+        //     //     // .Returns(rTokenDbSet.Object);
+        //     //     .Verifiable();
+
+        //     httpContextAccessor
+        //         .Setup(x => x.HttpContext.Request.Headers.ContainsKey(It.IsAny<string>()))
+        //         .Returns(true);
+
+        //     var sut = new AuthService
+        //                 (
+        //                     dataContext.Object,
+        //                     aTokenManage.Object,
+        //                     rTokenManager.Object,
+        //                     httpContextAccessor.Object
+        //                 );
+
+        //     await sut
+        //         .Awaiting(x => x.RefreshToken("refresh-token-value-2"))
+        //         .Should()
+        //         .ThrowAsync<AppException>()
+        //         .WithMessage("Token revoked.");
+        // }
     }
 }
